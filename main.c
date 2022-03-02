04bce289326e468227b74e1b6aadebc347d53b43
Nikolaeva Nataliya <sino.nata2003@mail.ru.github.com>
2022-03-02 16:44:48 +0700

    Fix main.c

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ struct triangle {
   float x1, x2, x3, y1, y2, y3;
 } tr;
 
-double chisla(char *str, int *i); 
+double numbers(char *str, int *i); 
 
 int main() {
   int i;
@@ -33,18 +33,18 @@ int main() {
   while (fgets(m3, N, file) != NULL) {
     if (strncmp(m1, m3, R) == 0) {
       i = 7;
-      c.x = chisla(m3, &i);
-      c.y = chisla(m3, &i);
-      c.r = chisla(m3, &i);
+      c.x = numbers(m3, &i);
+      c.y = numbers(m3, &i);
+      c.r = numbers(m3, &i);
       printf("Circle: (%0.0f %0.0f) R = %0.2f\n", c.x, c.y, c.r);
     } else if (strncmp(m2, m3, R) == 0) {
       i = 9;
-      tr.x1 = chisla(m3, &i);
-      tr.y1 = chisla(m3, &i);
-      tr.x2 = chisla(m3, &i);
-      tr.y2 = chisla(m3, &i);
-      tr.x3 = chisla(m3, &i);
-      tr.y3 = chisla(m3, &i);
+      tr.x1 = numbers(m3, &i);
+      tr.y1 = numbers(m3, &i);
+      tr.x2 = numbers(m3, &i);
+      tr.y2 = numbers(m3, &i);
+      tr.x3 = numbers(m3, &i);
+      tr.y3 = numbers(m3, &i);
       printf("Triangle: (%0.0f %0.0f) (%0.0f %0.0f) (%0.0f %0.0f)\n", tr.x1,
              tr.y1, tr.x2, tr.y2, tr.x3, tr.y3);
     } else {
@@ -56,7 +56,7 @@ int main() {
 
 
 
-double chisla(char *str, int *i)  
+double numbers(char *str, int *i)  
 {
   double result = 0;
   double div = 10;
